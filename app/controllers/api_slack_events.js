014171363e8fe599d8e67fdf3d9f1ec31170065a
David Thong Nguyen <thnguyen2808@gmail.com>
2017-03-21 17:44:12 -0400

    Event Subscription verification on slack

--- app/controllers/api_slack_events.js
+++ app/controllers/api_slack_events.js
@@ -3,6 +3,13 @@ var api_slack_events_router = express.Router();
 
 
 api_slack_events_router.post('/', function (req, res) {
+    if (req.query.challenge){
+        res.json({
+            "value": req.query.challenge
+        })
+    } else {
+        res.sendStatus(500);
+    }
 });
 
 module.exports = api_slack_events_router;
