904e8de53429b3b035a97b27d478611f01d15835
David Thong Nguyen <thnguyen2808@gmail.com>
2017-03-21 14:55:35 -0400

    Don't use dotenv in Heroku

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 // modules =================================================
-var express        = require('express');
-var app            = express();
-var bodyParser     = require('body-parser');
-var http           = require('http').Server(app);
-var dotenv         = require('dotenv');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var dotenv = require('dotenv');
 
 // configuration ===========================================
 
 //load environment variables,
 //either from .env files (development),
 //heroku environment in production, etc...
-dotenv.load();
+process.env.NODE_ENV == "production" ? "" : dotenv.load();
 
 // public folder for images, css,...
 app.use(express.static(__dirname + '/public'))
 
 //parsing
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); //for parsing url encoded
+app.use(bodyParser.urlencoded({extended: true})); //for parsing url encoded
 
 // view engine ejs
 app.set('view engine', 'ejs');
@@ -33,7 +33,7 @@ app.set('port', (process.env.PORT));
 require('./app/controllers/botkit');
 
 //START ===================================================
-http.listen(app.get('port'), function(){
-  console.log('listening on port ' + app.get('port'));
+http.listen(app.get('port'), function () {
+    console.log('listening on port ' + app.get('port'));
 });
 
