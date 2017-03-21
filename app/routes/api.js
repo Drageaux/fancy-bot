f553ee43c67c79107f52678fdac669f1039347aa
David Thong Nguyen <thnguyen2808@gmail.com>
2017-03-21 15:19:19 -0400

    Test output

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,9 +3,10 @@ var Request = require('request');
 module.exports = function (app) {
     app.post('/api/challenge', function (req, res) {
         console.log(req.body)
-        if (req.body.challenger) {
+        var payload = req.body.payload;
+        if (payload) {
             res.status(200).send({
-                'text': req.body.challenger
+                'text': req.body.payload
             })
         } else {
             res.sendStatus(500);
