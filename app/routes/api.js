5ab525e34756f08939eecd443b9adadca7007439
David Thong Nguyen <thnguyen2808@gmail.com>
2017-03-21 15:36:31 -0400

    Beautify print JSON is better in console

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,9 +2,9 @@ var Request = require('request');
 
 module.exports = function (app) {
     app.post('/api/challenge', function (req, res) {
-        console.log(req.body)
         var payload = req.body.payload;
         if (payload) {
+            console.log(JSON.parse(payload))
             res.status(200).send({
                 'text': req.body.payload
             })
