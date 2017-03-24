9b27b09a71d364a5828a17c711b76c7ba2de6a72
David Thong Nguyen <thnguyen2808@gmail.com>
2017-03-24 12:12:46 -0400

    Efficient catch-all code for 3 types of items that can contain reactions

--- app/controllers/api_slack_events.js
+++ app/controllers/api_slack_events.js
@@ -1,52 +1,47 @@
 var express = require('express');
 var api_slack_events_router = express.Router();
 
-var slack_api = require("./custom_slack_web_api")();
+var slack_api = require('./custom_slack_web_api')();
 var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/groot-gamify';
 var storage = require('../../config/botkit_mongo_storage')({mongoUri: mongoUri});
 
 api_slack_events_router.post('/', function (req, res) {
     // tally reactions added
-    if (req.body.event && req.body.event.type == "reaction_added") {
-        console.log("** EVENT TRIGGERED: reaction_added");
+    if (req.body.event && req.body.event.type == 'reaction_added') {
+        console.log('** EVENT TRIGGERED: reaction_added');
         var item = req.body.event.item;
-
-        if (item && item.type == "message") {
-            slack_api.reactions.get({
-                channel: item.channel,
-                timestamp: item.ts
-            }, function (err, response) {
-                console.log("** RESPONSE:\n------------\n", response, "\n------------");
-                var emojis = response.message.reactions;
-                var userId = response.message.user;
-                var totalCount = 0;
-                emojis.forEach(function (emoji) {
-                    totalCount += !isNaN(emoji.count) ? emoji.count : 0;
-                });
-                if (totalCount < 5) {
-                    res.sendStatus(200)
-                } else {
-                    addPoints(userId, totalCount, res);
-                }
-            })
-        } else if (item && item.type == "file") {
-            slack_api.reactions.get({
-                file: item.file
-            }, function (err, response) {
-                console.log("** RESPONSE:\n------------\n", response, "\n------------");
-                var emojis = response.file.reactions;
-                var userId = response.file.user;
-                var totalCount = 0;
-                emojis.forEach(function (emoji) {
-                    totalCount += !isNaN(emoji.count) ? emoji.count : 0;
-                });
-                if (totalCount < 5) {
-                    res.sendStatus(200)
-                } else {
-                    addPoints(userId, totalCount, res);
-                }
-            })
+        if (!item) {
+            console.log('err: no item found from reaction_added event');
+            res.sendStatus(500);
+            return;
         }
+
+        var options = {};
+        switch (item.type) {
+            case 'message':
+                options = {
+                    channel: item.channel,
+                    timestamp: item.ts
+                };
+            case 'file':
+                options = {file: item.file};
+            case 'file_comment':
+                options = {file_comment: item.file_comment}
+        }
+        slack_api.reactions.get(options, function (err, response) {
+            console.log('** RESPONSE:\n------------\n', response, '\n------------');
+            var emojis = response[item.type].reactions;
+            var userId = response[item.type].user;
+            var totalCount = 0;
+            emojis.forEach(function (emoji) {
+                totalCount += !isNaN(emoji.count) ? emoji.count : 0;
+            });
+            if (totalCount < 5) {
+                res.sendStatus(200)
+            } else {
+                addPoints(userId, totalCount, res);
+            }
+        });
     } else {
         res.sendStatus(500);
         return;
@@ -72,7 +67,7 @@ function addPoints(userId, points, res) {
             storage.users.save(savedUser, function (err, finalUser) {
                 if (err) console.log('err:', err);
                 else {
-                    console.log("User " + finalUser.id + " updated with score " + finalUser.score);
+                    console.log('User ' + finalUser.id + ' updated with score ' + finalUser.score);
                     res.json(finalUser);
                 }
             });
